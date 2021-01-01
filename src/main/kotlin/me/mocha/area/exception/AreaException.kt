9c359689cfb3e1f8396d769a6d14c2a63f73a88a
hwangseonu <hwangseonu12@naver.com>
2021-01-01 22:19:57 +0900

    for remake to forgemod

--- src/main/kotlin/me/mocha/area/exception/AreaException.kt
+++
@@ -1,6 +0,0 @@
-package me.mocha.area.exception
-
-import java.lang.RuntimeException
-
-open class AreaException(message: String) : RuntimeException(message) {
-}
--- src/main/kotlin/me/mocha/area/provider/Provider.kt
+++
@@ -1,9 +0,0 @@
-package me.mocha.area.provider
-
-import me.mocha.area.AreaInfo
-import org.bukkit.util.Vector
-
-interface Provider {
-    fun save()
-    fun createArea(id: Int, world: String, start: Vector, end: Vector): AreaInfo
-}
